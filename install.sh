411ef7051b5d24ab6135342a743211bb299fd2ac
camilotutoriales <46059590+camilotutoriales@users.noreply.github.com>
2019-03-04 06:58:53 -0500

    Archivo de las dependencias

---
+++ install.sh
@@ -0,0 +1,25 @@
+apt update && apt upgrade
+
+slepp 2.5
+
+pkg install git
+
+sleep 2.5
+
+pkg install figlet
+
+sleep 2.5
+
+pkg install util-linux
+
+sleep 2.5
+
+pkg install ruby
+
+sleep 2.5
+
+gem install lolcat
+
+sleep 2.5
+
+figlet +FIN+ | lolcat
